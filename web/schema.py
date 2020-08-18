54fa27e6a11f5c066aa2727b17f16b68f3fc3fee
Jovana <jjovana314@gmail.com>
2020-08-18 09:24:53 +0200

    schema modified

--- web/schema.py
+++ web/schema.py
@@ -2,20 +2,20 @@ movie_schema = {
     "type": "object",
     "properties": {
         "movie_title": {
-                "type": "string", "minLength": 2, "maxLength": 50
+            "type": "string", "minLength": 2, "maxLength": 50
         },
         "year_release": {
-                "type": "integer", "minimum": 1900, "maximum": 2021
+            "type": "integer", "minimum": 1900, "maximum": 2021
         },
         "rate": {
-                "type": "number", "minimum": 0.0, "maximum": 10.0
+            "type": "number", "minimum": 0.0, "maximum": 10.0
         },
         "director": {
-                "type": "string", "minLegth": 2, "maxLength": 30
+            "type": "string", "minLegth": 2, "maxLength": 30
         },
         "stars": {
-                "type": "array",
-                "contains": {"type": "string"}
+            "type": "array",
+            "items": {"type": "string"}
         }
     },
     "required": ["movie_title", "year_release", "rate"],
