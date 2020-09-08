5e618a519bc2360f7ba7cd46943d8a56e0b00252
Jovana <jjovana314@gmail.com>
2020-09-08 11:15:17 +0200

    post method modified

--- web/app.py
+++ web/app.py
@@ -23,36 +23,35 @@ class Movie(Resource):
                     "code": ex_m.BAD_REQUEST
                 }
             )
-        else:
-            for movie in movies:
-                try:
-                    helper.validate_schema(movie, movie_schema)
-                except exceptions.MovieSchemaError as ex:
-                    return jsonify({"message": ex.args[0], "code": ex.args[1]})
+        for movie in movies:
+            try:
+                helper.validate_schema(movie, movie_schema)
+            except exceptions.MovieSchemaError as ex:
+                return jsonify({"message": ex.args[0], "code": ex.args[1]})
 
-            # establish connection with database and save data
-            connection = sqlite3.connect("data.db")
-            cursor = connection.cursor()
-            create_table = """
-            CREATE TABLE IF NOT EXISTS movies (
-                id int, movie_title text, 
-                year_release int, rate real)
-            """
+        # establish connection with database and save data
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        create_table = """
+        CREATE TABLE IF NOT EXISTS movies (
+            id int, movie_title text, 
+            year_release int, rate real)
+        """
 
-            cursor.execute(create_table)
-            all_movies = helper.data_generate(movies)
-            insert_query = "INSERT INTO movies VALUES (?, ?, ?, ?)"
-            # here we can also insert one by one row with loop
-            cursor.executemany(insert_query, all_movies)
-            connection.commit()
-            connection.close()
+        cursor.execute(create_table)
+        all_movies = helper.data_generate(movies)
+        insert_query = "INSERT INTO movies VALUES (?, ?, ?, ?)"
+        # here we can also insert one by one row with loop
+        cursor.executemany(insert_query, all_movies)
+        connection.commit()
+        connection.close()
 
-            return jsonify(
-                {
-                    "message": "data saved in database",
-                    "code": ex_m.OK
-                }
-            )
+        return jsonify(
+            {
+                "message": "data saved in database",
+                "code": ex_m.OK
+            }
+        )
 
 
 api.add_resource(Movie, "/movie")
